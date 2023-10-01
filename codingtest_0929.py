e5870a33087f72df71f65ef38b9e78be84d7261d
yeeeeechan <leeyechan6@gmail.com>
2023-10-01 22:09:05 +0900

    study : coding test

--- codingtest_0929.py
+++ codingtest_0929.py
@@ -1,4 +1,4 @@
-# 화성 수학
+# 5355. 화성 수학
 # 소수점 둘째 자리까지 출력하는 round(실수, 2)
 
 @ = ?*3
@@ -52,6 +52,36 @@ functionD ()
 # for i in x[1:]:
 # 리스트의 길이에 따라 for문을 돌리는 횟수를 정하고 싶다면, for i ing range(len(x)-1):
 
+# print("{:.2f}".format(ans))
+# 원하는 포맷에 맞게 출력하기 :  출력할 소수점 이하 자리수를 지정할 때는 .자리수f 표현으로 지정한다.
+
+# for _ in range(n): -> 변수 _가 0,1,2,3...n의 값을 갖고 반복을 수행한다.
+
+n = int(input())
+
+for _ in range(n):
+  line = list(map(str, input().split()))
+  ans = line[0]
+  for i in range(len(line)):
+    if i == 0:
+      ans = float(line[i])
+    elif line[i] == '@':
+      ans *= 3
+    elif line[i] == '%':
+      ans += 5
+    elif line[i] == '#':
+      ans -= 7
+
+  print("{:.2f}".format(ans))
+
+
+
 # 문자열 반복
 
-s = input()
+n = int(input())
+
+for _ in range(n): # 입력받은 테스트 케이스의 수만큼 for문을 반복한다.
+  cnt, word = input().split()
+  for p in word:
+    print(p*int(cnt), end='') # end='' 문자열을 오른쪽으로 붙이며 출력
+  print() # for p in word 출력문이 종료되면 줄넘김
