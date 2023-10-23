3f5a0f9a84af0527f0230af19356efc0b225c484
yeeeeechan <leeyechan6@gmail.com>
2023-10-23 15:12:48 +0900

    9610, 8958 풀이 완료

--- ct_1023.py
+++ ct_1023.py
@@ -18,3 +18,46 @@ while True:
         break
 
     print(f1+f2)
+
+# 9610. 사분면
+dots = int(input())
+result1 = 0
+result2 = 0
+result3 = 0
+result4 = 0
+result5 = 0
+
+for i in range(dots):
+    x, y = map(int, input().split())
+    if x > 0 and y > 0:
+        result1 += 1
+    elif x < 0 and y > 0:
+        result2 += 1
+    elif x < 0 and y < 0:
+        result3 += 1
+    elif x > 0 and y < 0:
+        result4 += 1
+    elif x == 0 or y == 0:
+        result5 += 1
+
+print("Q1:", result1)
+print("Q2:", result2)
+print("Q3:", result3)
+print("Q4:", result4)
+print("AXIS:", result5)
+
+# 8958. OX 퀴즈
+case = int(input())
+
+for _ in range(case):
+    list = list(input())
+    score = 0
+    sum_score = 0
+
+    for ox in list:
+        if ox == "O":
+            score += 1
+            sum_score += score
+        else:
+            score = 0
+    print(sum_score)
