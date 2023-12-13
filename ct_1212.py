28d5be7373b883614977a147c578b4944ec33994
yeeeeechan <leeyechan6@gmail.com>
2023-12-13 23:11:01 +0900

    code format

--- ct_1212.py
+++ ct_1212.py
@@ -8,18 +8,18 @@ m_list = list(map(int, input().split()))
 n_list.sort()
 
 for i in m_list:
-  left, right = 0, n-1
-  isExist = 0
+    left, right = 0, n-1
+    isExist = 0
 
-  while left <= right:
-    mid = (left + right) // 2
-    if i == n_list[mid]:
-      isExist = 1
-      print(isExist)
-      break
-    elif i > n_list[mid]: # 요소가 중간값보다 크면
-      left = mid +1
-    else:
-      right = mid -1
-  if isExist == 0:
-    print(isExist)
+    while left <= right:
+        mid = (left + right) // 2
+        if i == n_list[mid]:
+            isExist = 1
+            print(isExist)
+            break
+        elif i > n_list[mid]:  # 요소가 중간값보다 크면
+            left = mid + 1
+        else:
+            right = mid - 1
+    if isExist == 0:
+        print(isExist)
